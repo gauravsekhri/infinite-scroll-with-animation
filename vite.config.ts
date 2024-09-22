b5a37d3eafc562496bfcffeef0bd4cb815a3dcdd
Gaurav Sekhri <gauravsekhri69@gmail.com>
2024-09-22 22:25:46 +0530

    added changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/infinite-scroll-with-animation",
   plugins: [react()],
   resolve: {
     alias: {
